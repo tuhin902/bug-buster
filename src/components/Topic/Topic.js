6a072828ad828fb9e94475933a4a30a9740cfcbc
tuhin902 <tuhin902md@gmail.com>
2022-10-12 22:03:02 +0600

    topic quiz added

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -11,10 +11,14 @@ const Topic = () => {
             <h1 className="text-3xl ml-20 text-orange-500 font-bold">
                 Quiz of :{topics.data.name}
             </h1>
-            {topics.data.questions.map((topic) => (
-                <TopicQuiz topic={topic} key={topic.id}
-                ></TopicQuiz>
-            ))}
+            {
+                topics.data.questions.map((topic) => (
+                    <TopicQuiz
+                        topic={topic}
+                        key={topic.id}
+                    ></TopicQuiz>
+                ))
+            }
         </div>
     );
 };
