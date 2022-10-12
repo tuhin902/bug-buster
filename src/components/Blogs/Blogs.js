36e89ea52b2b4482063726922572a6abc5e21252
tuhin902 <tuhin902md@gmail.com>
2022-10-12 17:15:21 +0600

    blog 3rd answer added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1 className="text-5xl text-center mt-16 font-bold text-lime-500">
+            <h1 className="text-5xl text-center mt-16 font-bold text-orange-600">
                 Our Queston is here
             </h1>
             <div className="ml-32 mt-32">
-                <div className="blog w-3/4 border-2 border-lime-400 rounded-lg p-6">
+                <div className="blog w-3/4 border-2 border-orange-500 rounded-lg p-6">
                     <h1 className="mb-4 text-3xl font-semibold">
                         Question 1: What Is The Purpose Of React Router?
                     </h1>
@@ -28,7 +28,7 @@ const Blogs = () => {
                         experience.
                     </p>
                 </div>
-                <div className="mt-12 blog  w-3/4 border-2 border-lime-400 rounded-lg p-6">
+                <div className="mt-12 blog  w-3/4 border-2 border-orange-600 rounded-lg p-6">
                     <h2 className="mb-4 text-3xl font-semibold">
                         {" "}
                         Question 2:How Does Context Api Work?
@@ -47,7 +47,25 @@ const Blogs = () => {
                         state.
                     </p>
                 </div>
-
+                <div className="mt-12 blog border-2 p-6 border-orange-500 rounded-lg  w-3/4">
+                    <h2 className="mb-4 text-3xl font-semibold">
+                        {" "}
+                        Question 3: What Is UseRef?
+                    </h2>
+                    <p className="letter">
+                        Ans: The useRef hook is the new addition in React 16.8. Before
+                        proceeding to this article there is a prerequisite to know about the
+                        ref in react. The useRef is a hook that allows to directly create a
+                        reference to the DOM element in the functional component.The useRef
+                        returns a mutable ref object. This object has a property called
+                        .current. The value is persisted in the refContainer.current
+                        property. These values are accessed from the current property of the
+                        returned object. The .current property could be initialised to the
+                        passed argument initialValue e.g. useRef(initialValue). The object
+                        can persist a value for a full lifetime of the component. Example:
+                        How to access the DOM using useRef hook.
+                    </p>
+                </div>
             </div>
         </div>
     );
